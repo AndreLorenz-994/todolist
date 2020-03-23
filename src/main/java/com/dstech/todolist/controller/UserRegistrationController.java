2045f2a758ed714db49560789fae2f8a43d387ff
AndreLorenz-994 <61145858+AndreLorenz-994@users.noreply.github.com>
2020-03-23 12:58:19 +0100

    error null There was an unexpected error (type=null, status=null).

--- src/main/java/com/dstech/todolist/controller/UserRegistrationController.java
+++ src/main/java/com/dstech/todolist/controller/UserRegistrationController.java
@@ -4,22 +4,16 @@ import javax.mail.MessagingException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dstech.todolist.dao.UserRegistrationDao;
 import com.dstech.todolist.model.User;
-import com.dstech.todolist.service.ImageService;
 import com.dstech.todolist.service.MailService;
 import com.dstech.todolist.service.UserService;
 
@@ -32,9 +26,6 @@ public class UserRegistrationController {
     
     @Autowired
 	private MailService mailService;
-    
-    @Autowired
-	private ImageService imageService;
 
     @ModelAttribute("user")
     public UserRegistrationDao userRegistrationDto() {
@@ -62,15 +53,5 @@ public class UserRegistrationController {
         mailService.sendMail(userDto.getEmail(), "Confirm registration", "User has been registered successfully");
 		return "index";
     }
-    
-    @GetMapping("/downloadFile/{id}")
-	public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Long id) {
-		// Load file from database
-		User dbFile = imageService.getFile(id);
-
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(dbFile.getTypeImage()))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; typeImage=\"" + dbFile.getEmail() + "\"")
-				.body(new ByteArrayResource(dbFile.getImage()));
-	}    
     
 }
